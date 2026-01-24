26171cb7ef2036a5bb1b1919414d19e94606f2a3
Prashijha123 <prashijha123@gmail.com>
2026-01-24 16:33:27 +0530

    21 Jan 2026

---
+++ src/ex_17_OOPs/Lab160_Constructor.java
@@ -0,0 +1,23 @@
+package ex_17_OOPs;
+
+public class Lab160_Constructor {
+    static void main(String[] args) {
+        Animal dog = new Animal();
+        MySQL a = new MySQL();
+        ReadExcelFile e = new ReadExcelFile();
+
+    }
+
+}
+
+class MySQL {
+    MySQL() {
+        System.out.println("MySQL Connected!");
+    }
+}
+
+class ReadExcelFile {
+    ReadExcelFile() {
+        System.out.println("ExcelFile is Loaded!");
+    }
+}
